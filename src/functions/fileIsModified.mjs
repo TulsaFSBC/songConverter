a423b2f2eccba1a45f90caa90b14c54f75a27123
MarkelL12 <mark.l.fsbc@gmail.com>
2024-11-27 22:56:43 -0600

    created working presentation string for v7

--- src/functions/fileIsModified.mjs
+++ src/functions/fileIsModified.mjs
@@ -8,6 +8,8 @@ import Colors from '../../node_modules/rtf/lib/colors.js'
 import Fonts from '../../node_modules/rtf/lib/fonts.js'
 import RTF from '../../node_modules/rtf/lib/rtf.js'
 import { v4 as uuidv4} from 'uuid';
+import { json } from "express";
+import * as child from 'child_process'
 
 //todo
 //remove rtf module
@@ -43,6 +45,7 @@ app.http('fileIsModified', {
     methods: ['GET','POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
+        const presentationVersion = 7;
         var accessToken, fileInfoResponse;
         const requestBody = await request.text();
         const requestData = JSON.parse(requestBody)
@@ -84,6 +87,7 @@ app.http('fileIsModified', {
             .catch((error) => context.error(error));
 
         const jsonFileInfo = JSON.parse(fileInfoResponse);
+        //context.log(jsonFileInfo)
         const downloadUrl = jsonFileInfo["@microsoft.graph.downloadUrl"];
         context.log("Downloading file...");
         const response = await fetch(downloadUrl)
@@ -93,9 +97,15 @@ app.http('fileIsModified', {
         context.log("File downloaded successfully.");
         
         context.log("Starting file conversion...")
-        const presentationHeader = fs.readFileSync('./pro6Templates/presentationHeader.txt').toString(),
-              presentationFooter = fs.readFileSync('./pro6Templates/presentationFooter.txt').toString(),
-              slideTemplate = fs.readFileSync('./pro6Templates/presentationSlide.txt').toString();
+        const pro6PresentationHeader = fs.readFileSync('./pro6Templates/presentationHeader.txt').toString(),
+              pro6PresentationFooter = fs.readFileSync('./pro6Templates/presentationFooter.txt').toString(),
+              pro6SlideTemplate = fs.readFileSync('./pro6Templates/presentationSlide.txt').toString();
+
+        const pro7PresentationTemplate = fs.readFileSync('./pro7Templates/presentationTemplate.txt').toString(),
+              pro7SlideTemplate = fs.readFileSync('./pro7Templates/slideTemplate.txt').toString(),
+              pro7SlideTextTemplate = fs.readFileSync('./pro7Templates/slideTextTemplate.txt').toString(),
+              pro7TextLineTemplate = fs.readFileSync('./pro7Templates/textLineTemplate.txt').toString()
+
         let config = {
             "preserveLineBreaks":true,
             "preserveOnlyMultipleLineBreaks":false,
@@ -105,7 +115,7 @@ app.http('fileIsModified', {
         async function extractTextFromPptx() {
             try {
                 pptxText = await new Promise((resolve, reject) => {
-                    textract.fromFileWithPath("./powerpoint.pptx", config, (error, text) => {
+                    textract.fromFileWithPath("./powerpoint1.pptx", config, (error, text) => {
                         if (error) {
                             reject(error);
                         } else {
@@ -113,49 +123,164 @@ app.http('fileIsModified', {
                         }
                     });
                 });
-                return pptxText; // Return the extracted text if needed
             } catch (error) {
                 console.error("Error extracting text:", error);
             }
         }
         await extractTextFromPptx();
-        context.log(pptxText)
         const textSlides = pptxText.split("\n\n");
+        var outputFilePath
+        if(presentationVersion == 6){
+            outputFilePath = `${(jsonFileInfo.name).replace(".pptx", ".pro6")}`
+            var pro6SlidesArray = []
+            textSlides.forEach(slide => {
+                if(slide != ""){
+                    let b64PlainText = b64(slide);
+                    let slideLines = slide.split("\n");
+                    let rtfSlideLinesArray = [];
+                    slideLines.forEach(line =>{
+                        if (line != ""){
+                            let rtfLine = ("{\\fs200\\outl0\\strokewidth-20\\strokec1\\f0 {\\cf3\\ltrch " + line + "}\\li0\\sa0\\sb0\\fi0\\qc\\par}")
+                            rtfSlideLinesArray.push(rtfLine)
+                        }
+                    })
+                    let rtfSlideLines = rtfSlideLinesArray.join("\\n")
+                    let rtfSlide = ("{\\rtf1\\prortf1\\ansi\\ansicpg1252\\uc1\\htmautsp\\deff2{\\fonttbl{\\f0\\fcharset0 Times New Roman;}}{\\colortbl;\\red0\\green0\\blue0;\\red255\\green255\\blue255;\\red250\\green235\\blue215;}\\loch\\hich\\dbch\\pard\\slleading0\\plain\\ltrpar\\itap0{\\lang1033\\fs100\\outl0\\strokewidth-20\\strokec1\\f2\\cf1 \\cf1\\qc \n" + rtfSlideLines + "\n } \n }");
+                    let b64RTFText = b64(rtfSlide)
+                    let pro6SlideString = pro6SlideTemplate;
+                    pro6SlideString = pro6SlideString.replace("$PLAIN_TEXT", b64PlainText); //plugs in actual content into slide template
+                    pro6SlideString = pro6SlideString.replace("$RTF_TEXT", b64RTFText);
+                    pro6SlideString = pro6SlideString.replace("$SLIDE_UUID", uuidv4());
+                    pro6SlideString = pro6SlideString.replace("$TEXTBOX_UUID", uuidv4());
+                    pro6SlidesArray.push(pro6SlideString)
+                }
+            })        
+
+            const presentationString = pro6PresentationHeader + pro6SlidesArray.join() + pro6PresentationFooter;
+            fs.writeFileSync(`./${outputFilePath}`, presentationString, err => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    context.log("pro6 File created successfully.")
+                }
+            });
+        } else if (presentationVersion == 7){
+            outputFilePath = `${(jsonFileInfo.name).replace(".pptx", ".pro")}`
+            var pro7SlidesArray = [];
+            textSlides.forEach(slide => {
+                if(slide != ""){
+                    let slideLines = slide.split("\n");
+                    let rtfSlideLinesArray = [];
+                    slideLines.forEach(line =>{
+                        if (line != ""){
+                            let rtfLine = pro7TextLineTemplate.replace("$TEXT", line)
+                            //rtfLine = rtfLine.replaceAll("\\\\", "\\")
+                            rtfSlideLinesArray.push(rtfLine)
+                        }
+                    })
+                    slideLines[slideLines.length - 1].replace(/\\par\\pard/gm, "")
+                    let rtfSlideLines = rtfSlideLinesArray.join(/*"\\n*/"");
+                    console.log(rtfSlideLines)                    
+                    let rtfSlide = pro7SlideTextTemplate.replace("$TEXT_LINES", rtfSlideLines);
+                    let pro7SlideString = pro7SlideTemplate.replace("$RTF_DATA", rtfSlide);
+                    pro7SlideString = pro7SlideString.replace(/\$UUID/gm, function(){
+                        return uuidv4()
+                    });
+                    pro7SlidesArray.push(pro7SlideString)
+                }
+            })        
+
+            var presentationString = pro7PresentationTemplate.replace("$PRESENTATION_NAME", "testinggg");
+            presentationString = presentationString.replace("$SLIDES", pro7SlidesArray.join("\n"));
+            presentationString = presentationString.replace(/\$UUID/gm, function(){
+                        return uuidv4()
+                    });
+            //presentationString = presentationString.replaceAll("\\", "\\\\")
+            fs.writeFileSync(`./presentationString1.txt`, presentationString, err => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    context.log("pro File created successfully.")
+                }
+            });
+            
+
+            /*await child.exec(`".\\protoc\\bin\\protoc.exe" --encode="rv.data.Presentation" --proto_path ".\\proto" ".\\proto\\propresenter.proto" < "${presentationString}" > ".\\${outputFile}"`, (err, stdout, stderr) => {
+                if (err) {
+                  // node couldn't execute the command
+                  return;
+                }
+              
+                // the *entire* stdout and stderr (buffered)
+                console.log("I've been here!")
+                console.log(`stdout: ${stdout}`);
+                console.log(`stderr: ${stderr}`);
+
+              }); */
+              
+                const outputFile = fs.createWriteStream(`./${outputFilePath}`);
+                const inputFile = fs.createReadStream("./presentationString.txt")
+                const protoc = child.spawn('.\\protoc\\bin\\protoc.exe', [
+                '--encode=rv.data.Presentation',
+                '--proto_path=./proto',
+                './proto/propresenter.proto'
+                ]);
+
+                // Pipe input and output
+                inputFile.pipe(protoc.stdin);
+                protoc.stdout.pipe(outputFile);
+
+                protoc.on('close', (code) => {
+                    console.log(`Process exited with code: ${code}`);
+                });
+
+                protoc.stderr.on('data', (data) => {
+                    console.error(`stderr: ${data}`);
+                });
+
+        }
         
-        var pro6SlidesArray = []
-        textSlides.forEach(slide => {
-            if(slide != ""){
-                let b64PlainText = b64(slide);
-                let slideLines = slide.split("\n");
-                let rtfSlideLinesArray = [];
-                slideLines.forEach(line =>{
-                    if (line != ""){
-                        let rtfLine = ("{\\fs200\\outl0\\strokewidth-20\\strokec1\\f0 {\\cf3\\ltrch " + line + "}\\li0\\sa0\\sb0\\fi0\\qc\\par}")
-                        rtfSlideLinesArray.push(rtfLine)
+/*
+        context.log("Uploading file to SharePoint...")
+        const destinationFolder = ((jsonFileInfo.parentReference.path).split("root:/")[0]) + "root:/proPresenter Files"
+        var fileName = outputFile;
+        var destinationPath;
+        context.log("Destination path: " + destinationPath)
+        var isNameUnique = false;
+        var i = 1;
+        while (!isNameUnique) {
+            destinationPath = destinationFolder //+ fileName;
+            await fetch(`https://graph.microsoft.com/v1.0/${destinationPath}`,{ // check if file exists
+                method: "GET",
+                headers:{
+                    "Authorization": `Bearer ${accessToken}`,
+                    "Accept": "application/json"
+                }
+            })
+                .then((response) => response.text())
+                .then((result) => {
+                    console.log(result)
+                    if(result == '{"error":{"code":"itemNotFound","message":"The resource could not be found."}}'){
+                        context.log("File name is unique. Uploading the file...")
+                        isNameUnique = true;
+                    }else{
+                        context.log("File name is not unique.")
+                        if(i == 1){
+                            fileName += fileName + `( ${i})`
+                        }else{
+                            if(presentationVersion == 6){
+                                fileName = fileName.replace(/ (.*?).pro6/, ` (${i}).pro6`)
+                            } else if (presentationVersion == 7){
+                                fileName = fileName.replace(/ (.*?).pro/, ` (${i}).pro`)
+                            }
+                            
+                        }
+                        i++;
                     }
                 })
-                let rtfSlideLines = rtfSlideLinesArray.join("\\n")
-                let rtfSlide = ("{\\rtf1\\prortf1\\ansi\\ansicpg1252\\uc1\\htmautsp\\deff2{\\fonttbl{\\f0\\fcharset0 Times New Roman;}}{\\colortbl;\\red0\\green0\\blue0;\\red255\\green255\\blue255;\\red250\\green235\\blue215;}\\loch\\hich\\dbch\\pard\\slleading0\\plain\\ltrpar\\itap0{\\lang1033\\fs100\\outl0\\strokewidth-20\\strokec1\\f2\\cf1 \\cf1\\qc \n" + rtfSlideLines + "\n } \n }");
-                let b64RTFText = b64(rtfSlide)
-                let pro6SlideString = slideTemplate;
-                pro6SlideString = pro6SlideString.replace("$PLAIN_TEXT", b64PlainText); //plugs in actual content into slide template
-                pro6SlideString = pro6SlideString.replace("$RTF_TEXT", b64RTFText);
-                pro6SlideString = pro6SlideString.replace("$SLIDE_UUID", uuidv4());
-                pro6SlideString = pro6SlideString.replace("$TEXTBOX_UUID", uuidv4());
-                pro6SlidesArray.push(pro6SlideString)
-            }
-        })        
-
-        const presentationString = presentationHeader + pro6SlidesArray.join() + presentationFooter;
-
-        fs.writeFile('./pro6.pro6', presentationString, err => {
-            if (err) {
-                console.error(err);
-            } else {
-                context.log("pro6 File created successfully.")
-            }
-        });
-
+                .catch((error) => context.error(error));
+        }
+        */
         return { body: `This worked!` };
     }
 });
